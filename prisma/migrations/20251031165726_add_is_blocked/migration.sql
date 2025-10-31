d2894d04029030c63c474b5381ef5a9f4e0ce481
Cofeek <kormyshev11@mail.ru>
2025-10-31 22:00:05 +0500

    [database]: add `isBlocked` property to `User`

---
+++ prisma/migrations/20251031165726_add_is_blocked/migration.sql
@@ -0,0 +1,18 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "fullName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'USER',
+    "status" TEXT NOT NULL DEFAULT 'ACTIVE',
+    "isBlocked" BOOLEAN NOT NULL DEFAULT false
+);
+INSERT INTO "new_User" ("email", "fullName", "id", "password", "role", "status") SELECT "email", "fullName", "id", "password", "role", "status" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
